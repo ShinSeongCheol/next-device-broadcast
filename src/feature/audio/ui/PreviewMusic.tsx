ae8ca23104ebbc78e40753b4161fb8d8c8b48fe3
shinseongcheol <cher3725@gmail.com>
2026-07-23 17:43:31 +0900

    :sparkles: '홈'
    - 미리듣기 기능 구현

---
+++ src/feature/audio/ui/PreviewMusic.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import {AspectRatio} from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {Play} from "lucide-react";
+import {WaveForm} from "@/src/feature/audio";
+import {AudioDetail} from "@/src/repository/audio";
+import {useState} from "react";
+
+interface Props {
+    audioDetails: AudioDetail[]
+}
+
+export default function PreviewMusic({audioDetails}: Props) {
+
+    const [uuid, setUuid] = useState<string|null>(null);
+
+    const handleClick = (uuid:string) => {
+        setUuid(uuid);
+    }
+
+    return (
+        <div className={'flex flex-col gap-4'}>
+            <div className={'flex items-center justify-between'}>
+                <h1 className={'text-4xl font-bold'}>다시 듣기</h1>
+                <Button variant={'outline'}>더보기</Button>
+            </div>
+
+            <div className={'max-w-7xl overflow-x-auto whitespace-nowrap'}>
+                <div className={'inline-flex gap-4'}>
+                    {audioDetails.map((audioDetail) => {
+                        return (
+                            <div key={audioDetail.id} className={'flex flex-col gap-2 w-full max-w-48 min-w-0'}>
+                                <AspectRatio ratio={1} className="relative w-full rounded-lg bg-neutral-800 group flex items-center justify-center" onClick={() => handleClick(audioDetail.uuid)}>
+                                    <Image src={`/api/images?audioId=${audioDetail.id}`} alt={audioDetail.audioCommon?.title ?? ""} fill sizes={'100%'} loading={'eager'} className="rounded-lg object-cover group-hover:cursor-pointer group-hover:opacity-80" />
+                                    <Play fill={'white'} className={'absolute stroke-none opacity-0 group-hover:opacity-100 group-hover:cursor-pointer'}/>
+                                </AspectRatio>
+
+                                <div className={'w-full min-w-0 overflow-hidden'}>
+                                    <p className={'font-semibold line-clamp-2 overflow-hidden break-all'}>
+                                        {audioDetail.audioCommon?.title || ''}
+                                    </p>
+                                    <p className={'text-sm text-neutral-600 truncate'}>
+                                        {audioDetail.audioCommon?.artist || ''}
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+            {uuid && <WaveForm key={uuid} uuid={uuid}/>}
+        </div>
+    )
+}
---
+++ src/feature/audio/ui/WaveForm.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import {useEffect, useRef} from "react";
+import WaveSurfer from "wavesurfer.js";
+
+interface Props {
+    uuid: string|null,
+}
+
+export default function WaveForm({uuid}: Props) {
+    const divRef = useRef<HTMLDivElement>(null)
+    const wavesurferRef = useRef<WaveSurfer | null>(null);
+
+    useEffect(() => {
+
+        if (!divRef.current) return;
+
+        if (wavesurferRef.current) {
+            wavesurferRef.current.destroy();
+        }
+
+        const options = {
+            container: divRef.current,
+            waveColor: '#818cf8',
+            progressColor: '#4f46e5',
+            url: `/api/audio/${uuid}`,
+            mediaControls: true,
+        }
+
+        wavesurferRef.current = WaveSurfer.create(options)
+
+        wavesurferRef.current.on('click', async () => {
+            if (!wavesurferRef.current) return;
+            await wavesurferRef.current.play();
+        })
+
+        return () => {
+            if (wavesurferRef.current) {
+                wavesurferRef.current.destroy();
+                wavesurferRef.current = null;
+            }
+
+        }
+
+    }, [uuid])
+
+    return (
+        <div ref={divRef}></div>
+    )
+}
